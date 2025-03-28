1fdc6d2082eceb2afc7cce0a6a24db17710130be
Jen1der <jenwunder10@gmail.com>
2025-03-27 21:49:42 -0500

    Update and rename script.js to js/chatbot.js
    new name and new code

---
+++ js/chatbot.js
@@ -0,0 +1,34 @@
+window.botpress.on("webchat:ready", () => {
+    window.botpress.open();
+});
+
+window.botpress.init({
+    "botId": "0d3d94b4-0bdb-4bcc-9e35-e21194ed2c1e",
+    "configuration": {
+        "composerPlaceholder": "Let's go, Hoots!",
+        "botName": "iHooty",
+        "botAvatar": "",
+        "website": {
+            "title": "https://ofallonhoots.com/",
+            "link": "https://ofallonhoots.com/"
+        },
+        "email": {
+            "title": "Ofallonhoots@prospectleague.com",
+            "link": "mailto:Ofallonhoots@prospectleague.com"
+        },
+        "phone": {
+            "title": "6367414668",
+            "link": "tel:6367414668"
+        },
+        "termsOfService": {},
+        "privacyPolicy": {},
+        "color": "#ffc53d",
+        "variant": "solid",
+        "themeMode": "light",
+        "fontFamily": "ibm",
+        "radius": 1
+    },
+    "clientId": "44c58e23-012d-4aa6-9617-abb818a66b42",
+    "selector": "#webchat"
+});
+
